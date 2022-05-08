8c241e02ced615edd342b5fbac3fe5a90cdfca72
JuanGarciaSantos <alu0101325583@ull.edu.es>
2022-05-08 20:47:19 +0100

    Reading files into problem class and creating tables

---
+++ MDP-DAA/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace MDP_DAA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string folderPath = Path.Combine(Directory.GetParent(workingDirectory).Parent.Parent.FullName, @"instances\");
+
+            foreach (string filePath in Directory.GetFiles(folderPath, "*.txt"))
+            {
+
+                Problem problema = new Problem(filePath);
+            }
+        }
+    }
+}
