b99d82bf28d4ff011ba28ba8e372e4d50474994d
JuanGarciaSantos <alu0101325583@ull.edu.es>
2022-05-20 16:12:02 +0100

    Fixed Grasp

--- MDP-DAA/LocalSearch.cs
+++ MDP-DAA/LocalSearch.cs
@@ -8,7 +8,8 @@ namespace MDP_DAA
     {
         Problem problem;
         public Solution Search(Solution solution)
-        {            
+        {
+            Solution newSol = new Solution(solution);
             bool improve;
             int firstIndex;
             int secondIndex;
@@ -16,33 +17,34 @@ namespace MDP_DAA
             do
             {
                 Problem leftElements = this.problem - solution;
+                
                 improve = false;
                 firstIndex = -1;
                 secondIndex = -1;
 
-                for (int i = 0; i < solution.nbElements; i++)
+                for (int i = 0; i < newSol.nbElements; i++)
                 {
                     for (int j = 0; j < leftElements.nbElements; j++)
                     {
-                        double newDistance = solution.SimulateDistance(leftElements.set[j], i);
+                        double newDistance = newSol.SimulateDistance(leftElements.set[j], i);
                         if (newDistance > bestDistance)
                         {
                             bestDistance = newDistance;
                             firstIndex = j;
-                            secondIndex = i;
+                            secondIndex = i;                            
                         }
                     }
                 }
                 if (firstIndex != -1 && secondIndex != -1)
                 {
-                    solution.diversity = bestDistance;
-                    (leftElements.set[firstIndex], solution.set[secondIndex]) = (solution.set[secondIndex], leftElements.set[firstIndex]);
-                    improve = true;
+                    newSol.diversity = bestDistance;
+                    (leftElements.set[firstIndex], newSol.set[secondIndex]) = (newSol.set[secondIndex], leftElements.set[firstIndex]);
+                    improve = true;                 
                 }
-
+                
             } while (improve);
 
-            return solution;            
+            return newSol;            
         }
         public LocalSearch(Problem problem)
         {
